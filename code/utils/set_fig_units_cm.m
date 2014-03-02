bb7276263a2e8f7c4cd8e2e873d708b4705e274e
David Duvenaud <dkd23@cam.ac.uk>
2014-03-02 23:28:34 +0000

    Added nice new figures to kernel chapter

--- code/utils/set_fig_units_cm.m
+++ code/utils/set_fig_units_cm.m
@@ -2,4 +2,4 @@ function set_fig_units_cm( width, height )
 
 set(gcf,'units','centimeters');
 pos = get(gcf,'position');
-set(gcf,'position',[pos(1:2),width .* 1.4 ,height .* 1.4]);
+set(gcf,'position',[pos(1:2),width,height]);
